e0f78ebf7f2167c428f4bc7e6314ba1d4a9651e4
pmsa29 <pmsa_29_88@hotmail.com>
2021-06-16 17:43:25 -0500

    End Of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.DATA
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
